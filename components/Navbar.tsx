ad00daae3412c8bba649ba0598236016fea2a68e
jydeveloper <lukaryu@naver.com>
2022-07-12 15:57:20 +0900

    @react-oauth/google로 구글로그인 대체, 가입 프로세스

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,41 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils';
 
 const Navbar = () => {
-  return (
-    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
-      <Link href="/">
-        <div className='w-[100px] md:w-[130px]'>
-          <Image
-            className="cursor-pointer"
-            src={Logo}
-            alt="TikTik"
-            layout="responsive"
-          />
-        </div>
-      </Link>
-    </div>
-  )
-}
+	const user = false;
+	return (
+		<div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+			<Link href="/">
+				<div className="w-[100px] md:w-[130px]">
+					<Image className="cursor-pointer" src={Logo} alt="TikTik" layout="responsive" />
+				</div>
+			</Link>
 
-export default Navbar
+			<div>SEARCH</div>
+
+			<div>
+				{user ? (
+					<div>Logged In</div>
+				) : (
+					<GoogleLogin
+						onSuccess={(response) => {
+							createOrGetUser(response);
+						}}
+						onError={() => console.log('Error')}
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
