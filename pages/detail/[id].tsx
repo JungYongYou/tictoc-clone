4d02c389ea007460d94caf478fd15cc08f8704ba
jydeveloper <lukaryu@naver.com>
2022-07-13 13:41:23 +0900

    likebutton 추가

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -9,6 +9,9 @@ import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 import axios from 'axios';
 import { BASE_URL } from '../../utils';
 import { Video } from '../../types';
+import useAuthStore from '../../store/authStore';
+import LikeButton from '../../components/LikeButton';
+import Comments from '../../components/Comments';
 
 interface IProps {
 	postDetails: Video;
@@ -19,6 +22,8 @@ const Detail = ({ postDetails }: IProps) => {
 	const [playing, setPlaying] = useState(false);
 	const [isVideoMuted, setIsVideoMuted] = useState(false);
 	const videoRef = useRef<HTMLVideoElement>(null);
+	const router = useRouter();
+	const { userProfile }: any = useAuthStore();
 
 	const onVideoClick = () => {
 		if (playing) {
@@ -36,13 +41,25 @@ const Detail = ({ postDetails }: IProps) => {
 		}
 	}, [post, isVideoMuted]);
 
+	const handleLike = async (like: boolean) => {
+		if (userProfile) {
+			const { data } = await axios.put(`${BASE_URL}/api/like`, {
+				userId: userProfile._id,
+				postId: post._id,
+				like,
+			});
+
+			setPost({ ...post, likes: data.likes });
+		}
+	};
+
 	if (!post) return null;
 
 	return (
 		<div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
 			<div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeact bg-cover bg-center">
 				<div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
-					<p>
+					<p className="cursor-pointer" onClick={() => router.back()}>
 						<MdOutlineCancel className="text-white text-[35px]" />
 					</p>
 				</div>
@@ -58,7 +75,6 @@ const Detail = ({ postDetails }: IProps) => {
 						)}
 					</div>
 				</div>
-
 				<div className="absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer">
 					{isVideoMuted ? (
 						<button onClick={() => setIsVideoMuted(false)}>
@@ -69,6 +85,36 @@ const Detail = ({ postDetails }: IProps) => {
 							<HiVolumeUp className="text-white text-2xl lg:text-4xl" />
 						</button>
 					)}
+				</div>
+			</div>
+			<div className="relative w-[1000px] md:w-[900px] lg:w-[700px]">
+				<div className="lg:mt-20 mt-10">
+					<div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+						<div className="ml-4 md:w-20 md:h-20 w-16 h-16">
+							<Link href="/">
+								<>
+									<Image width={62} height={62} className="rounded-full" src={post.postedBy.image} alt="profile photo" layout="responsive" />
+								</>
+							</Link>
+						</div>
+						<div>
+							<Link href="/">
+								<div className="mt-3 flex flex-col gap-2">
+									<p className="flex gap-2 items-center md:text-md font-bold text-primary">
+										{post.postedBy.userName}
+										{` `}
+										<GoVerified className="text-blue-400 text-md" />
+									</p>
+									<p className="capitalize font-medium text-xs text-gray-500 hidden md:block">{post.postedBy.userName}</p>
+								</div>
+							</Link>
+						</div>
+					</div>
+					<p className="px-10 text-lg text-gray-600">{post.caption}</p>
+					<div className="mt-10 px-10">
+						{userProfile && <LikeButton likes={post.likes} handleLike={() => handleLike(true)} handleDislike={() => handleLike(false)} />}
+					</div>
+					<Comments />
 				</div>
 			</div>
 		</div>
