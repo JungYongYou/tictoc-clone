305aacf786a44743f39171fe03ad9bcdfa3508a6
jydeveloper <lukaryu@naver.com>
2022-08-01 14:20:50 +0900

    fetchAllUsers 통해 SuggestedAccounts 추가

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const NoResults = ({ text }: IProps) => {
 	return (
 		<div className="flex flex-col justify-center items-center h-full w-full">
-			<p className="text-8xl">{text === 'No comments yet!' ? <BiCommentX /> : <MdOutlineVideocamOff />}</p>
+			<p className="text-8xl">{text === 'No Comments yet!' ? <BiCommentX /> : <MdOutlineVideocamOff />}</p>
 			<p className="text-2xl text-center">{text}</p>
 		</div>
 	);
