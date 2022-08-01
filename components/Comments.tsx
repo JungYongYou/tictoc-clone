a5f5922aedc864b77df1889d19b0544e21830255
jydeveloper <lukaryu@naver.com>
2022-08-01 14:49:19 +0900

    코멘트 보완

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,6 +5,7 @@ import { GoVerified } from 'react-icons/go';
 
 import useAuthStore from '../store/authStore';
 import NoResults from './NoResults';
+import { IUser } from '../types';
 
 interface IProps {
 	isPostingComment: boolean;
@@ -22,12 +23,47 @@ interface IComment {
 }
 
 const Comments = ({ comment, setComment, addComment, comments, isPostingComment }: IProps) => {
-	const { userProfile } = useAuthStore();
+	const { userProfile, allUsers } = useAuthStore();
 
 	return (
 		<div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
 			<div className="overflow-scroll lg:h-[475px]">
-				{comments?.length ? <div>{comments.map((item) => item.comment)}</div> : <NoResults text="No comments yet!" />}
+				{comments?.length ? (
+					<div>
+						{comments.map((item, idx) => (
+							<>
+								{allUsers?.map(
+									(user: IUser) =>
+										user._id === (item.postedBy._id || item.postedBy._ref) && (
+											<div className="p-2 items-center" key={idx}>
+												<Link href={`/profile/${user._id}`}>
+													<div className="flex items-start gap-3">
+														<div className="flex gap-3 hover:bg-primary p-2 cursor-pointer font-semibold rounded">
+															<div className="w-8 h-8">
+																<Image src={user.image} width={34} height={34} className="rounded-full" alt="user profile" layout="responsive" />
+															</div>
+															<div className="hidden xl:block">
+																<p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+																	{user.userName.replaceAll(' ', '')}
+																	<GoVerified className="text-blue-400" />
+																</p>
+																<p className="capitalize text-gray-400 text-xs">{user.userName}</p>
+															</div>
+														</div>
+													</div>
+												</Link>
+												<div>
+													<p>{item.comment}</p>
+												</div>
+											</div>
+										),
+								)}
+							</>
+						))}
+					</div>
+				) : (
+					<NoResults text="No comments yet!" />
+				)}
 			</div>
 			{userProfile && (
 				<div className="absolute bottom-0 left-0 pb-6 px-2 md:px-10">
